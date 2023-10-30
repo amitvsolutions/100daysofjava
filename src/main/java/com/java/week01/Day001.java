a35a4597ad8b5a75c3426d4aeb0170b948c30e7d
amitverma <amitverma.ibmindia@gmail.com>
2023-10-31 00:32:15 +0530

    Find minimum and maximum number from an Array

---
+++ src/main/java/com/java/week01/Day001.java
@@ -0,0 +1,44 @@
+package com.java.week01;
+import java.util.Arrays;
+
+public class Day001{
+	
+	public static void main(String[] args){
+		int[] items = { 10, 0, 30, 2, 7, 5, 90, 76, 100, 45, 55 };   // Min = 0, Max = 100
+		
+		// Approach-1
+		int max = items[0];
+		int min = items[0];
+
+		for (int i = 1; i < items.length; i++) {
+			if(items[i]>max){
+				max= items[i];
+			}else if(items[i]<min){
+				min =items[i];
+			}
+		}
+		System.out.println(max);	//100
+		System.out.println(min);	//0
+		
+		// Approach-2
+		max = Arrays.stream(items).max().getAsInt(); // 100
+		min = Arrays.stream(items).min().getAsInt(); // 0
+		
+		System.out.println(max);	//100
+		System.out.println(min);	//0
+		
+		// Approach-3: Try with Recursion !!!
+		
+	}
+	
+	// Puesdocode for Approach-1
+	/*Initialize the max and min with first item in the array
+          Iterate the array from second position (index 1)
+	  Compare the ith item with max and min
+	    if current item is greater than max
+		set max = current item
+	    elseif current item is lower than min
+	 	set min = current item*/
+
+}
+
